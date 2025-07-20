0b8b977ab25775c6562cff7453ac47c58b67203a
RaihanKabir277 <raihank0198@gmail.com>
2025-07-20 07:49:38 +0600

    add a text input on the screen

--- main.py
+++ main.py
@@ -9,10 +9,16 @@ screen.addshape(image)
 
 turtle.shape(image)
 
-def get_mouse_click_coor(x,y):
-    print(x, y)
+# ---------retrive x,y coordinate from images ---------
+# def get_mouse_click_coor(x,y):
+#     print(x, y)
 
-turtle.onscreenclick(get_mouse_click_coor)
+# turtle.onscreenclick(get_mouse_click_coor)
+# ------------   end      --------------------------
+
+answer_state = screen.textinput(title="Guess the state", prompt="Whats the another state's name?")
+# print(answer_state)
+
 
 turtle.mainloop()   #it is the alternate version of screen.exitonclick()
 
